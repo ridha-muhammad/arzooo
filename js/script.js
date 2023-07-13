e2d169302b1501b419d2a7825869607f0c75cbfc
Ridha Muhammad <ridha.mohd01@gmail.com>
2023-07-14 01:49:40 +0530

    commit no swiper

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     slidesPerGroup: 3,
+//     loop: true,
+//     loopFillGroupWithBlank: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextE1: ".swiper-button-next",
+//       prevE1: ".swiper-button-prev",
+//     },
+// });
+
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
